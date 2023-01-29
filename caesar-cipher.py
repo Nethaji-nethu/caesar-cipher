92691b06cd8ab8c4e3355d3d707fdd67b9090f97
Nethaji M Gowda <54547302+Nethaji-nethu@users.noreply.github.com>
2023-01-29 13:15:25 +0530

    second commit

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -7,6 +7,6 @@ def caesar(rotate_string, number_to_rotate_by):
 	#print(map_uppercase)
 	return rotate_string.translate(str.maketrans(string.ascii_lowercase,map_lowercase)).translate(str.maketrans(string.ascii_uppercase,map_uppercase))
 
-input_text = input('Enter the cipher / plain text to decode / encode :')
-key = int(input('enter the key to encode (1 to 26) / decode (-1 to -26)'))
+input_text = input('Enter the cipher / plain text to decode / encode : ')
+key = int(input('enter the key to encode (1 to 26) / decode (-1 to -26) : '))
 print(caesar(input_text,key))
